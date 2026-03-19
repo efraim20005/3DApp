76f23cc3343b1f3ff2f3e46757bdd654629525f1
efraim waruwu <efraimwaruwu62@gmail.com>
2026-03-19 10:35:22 +0700

    first commit

---
+++ src/components/About.jsx
@@ -0,0 +1,68 @@
+import React from "react";
+import Tilt from "react-tilt";
+import { motion } from "framer-motion";
+
+import { styles } from "../styles";
+import { services } from "../constants";
+import { SectionWrapper } from "../hoc";
+import { fadeIn, textVariant } from "../utils/motion";
+
+const ServiceCard = ({ index, title, icon }) => (
+  <Tilt className='xs:w-[250px] w-full'>
+    <motion.div
+      variants={fadeIn("right", "spring", index * 0.5, 0.75)}
+      className='w-full green-pink-gradient p-[1px] rounded-[20px] shadow-card'
+    >
+      <div
+        options={{
+          max: 45,
+          scale: 1,
+          speed: 450,
+        }}
+        className='bg-tertiary rounded-[20px] py-5 px-12 min-h-[280px] flex justify-evenly items-center flex-col'
+      >
+        <img
+          src={icon}
+          alt='web-development'
+          className='w-16 h-16 object-contain'
+        />
+
+        <h3 className='text-white text-[20px] font-bold text-center'>
+          {title}
+        </h3>
+      </div>
+    </motion.div>
+  </Tilt>
+);
+
+const About = () => {
+  return (
+    <>
+      <motion.div variants={textVariant()}>
+        <p className={styles.sectionSubText}>Introduction</p>
+        <h2 className={styles.sectionHeadText}>Overview.</h2>
+      </motion.div>
+
+      <motion.p
+        variants={fadeIn("", "", 0.1, 1)}
+        className='mt-4 text-secondary text-[17px] max-w-3xl leading-[30px]'
+      >
+        I'm a guy who dreams of creating change in lives through the lines of code I write.
+
+I enjoy working independently. In between activities, I often listen to podcasts and watch anime to keep me focused and avoid boredom. My interest in programming began to develop during my vocational high school years, especially during an internship at Radio Republik Indonesia (RRI). From there, I learned firsthand about the world of work, technology, and responsibility.
+
+In addition to formal education, I also learn independently through various open classes, such as programming classes and free English classes. This has helped me broaden my horizons and improve my technical and communication skills.
+
+I have considerable experience in computer networking and am familiar with using several Kali Linux tools for learning and exploring network security.
+      </motion.p>
+
+      <div className='mt-20 flex flex-wrap gap-10'>
+        {services.map((service, index) => (
+          <ServiceCard key={service.title} index={index} {...service} />
+        ))}
+      </div>
+    </>
+  );
+};
+
+export default SectionWrapper(About, "about");
